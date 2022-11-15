86579ba9145f34fe425bd4ab1a93e8316aa758d0
Yusupov-Toshpolat <toshpolaty@gmail.com>
2022-11-15 18:15:57 +0500

    add gateway server

---
+++ gateway-server/src/test/java/uz/nt/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.nt.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
